5c77a4b6400db1a9008164577c0e8fc8df49c9bf
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-12-13 18:51:10 +0200

    3.3

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 const express = require("express");
-const PORT = 3001;
 
-const data = [
+const persons = [
   { id: 1, name: "Arto Hellas", number: "0123456" },
   { id: 2, name: "Ada Lovelace", number: "39-44-67843" },
   { id: 3, name: "Dan Abramov", number: "12-43-578543" },
   { id: 4, name: "Mary Poppendick", number: "39-23-23478" },
 ];
 
+const PORT = 3001;
+
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/persons", (req, res) => {
-  res.json(data);
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 
 app.get("/info", (req, res) => {
   res.send(
     `<div>
-      <p>Phonebook has info for ${data.length} people</p>
+      <p>Phonebook has info for ${persons.length} people</p>
       <p>${new Date().toString()}</p>
     </div>`
   );
